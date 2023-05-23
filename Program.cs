f38cc0dfb2e858d34711669bd424c315e5b2ef22
Tomo4kap <tomo4kap@gmail.ru>
2023-05-23 13:22:31 +0300

    Homework Less 9 - 64

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Clear();
+
+int NaturalNumber(int n, int m)
+{
+    if (n == m) return n;
+    else Console.Write($"{NaturalNumber(n, m + 1)}, ");
+    return m;
+}
+Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 1)
+{
+    Console.WriteLine("Ввдите положительное число!");
+    return;
+}
+Console.WriteLine(NaturalNumber(n, 1));
